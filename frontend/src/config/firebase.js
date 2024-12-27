09b2390c0b4a578b9a3b58de4bc7bb17f316479d
MirasAshk <mirasAshk@gmail.com>
2024-12-27 17:04:40 +0000

    Config changes for prod

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -3,32 +3,30 @@ import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
-    // Your Firebase config object here
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	// Your Firebase config object here
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const auth = getAuth(app);
 
-if (
-    location.hostname === 'localhost' ||
-    location.hostname === '127.0.0.1'
-) {
-    // Point to the Storage emulator running on localhost.
-    // connectStorageEmulator(
-    //     storage,
-    //     import.meta.env.VITE_EMULATOR_HOST,
-    //     import.meta.env.VITE_STORAGE_EMULATOR_PORT
-    // );
-    connectStorageEmulator(storage, import.meta.env.VITE_EMULATOR_HOST, import.meta.env.VITE_STORAGE_EMULATOR_PORT);
-    connectAuthEmulator(
-        auth,
-        import.meta.env.VITE_AUTH_EMULATOR_PORT
-    );
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+	// Point to the Storage emulator running on localhost.
+	// connectStorageEmulator(
+	//     storage,
+	//     import.meta.env.VITE_EMULATOR_HOST,
+	//     import.meta.env.VITE_STORAGE_EMULATOR_PORT
+	// );
+	connectStorageEmulator(
+		storage,
+		import.meta.env.VITE_EMULATOR_HOST,
+		import.meta.env.VITE_STORAGE_EMULATOR_PORT
+	);
+	connectAuthEmulator(auth, import.meta.env.VITE_AUTH_EMULATOR_PORT);
 }
 
 export { auth, storage };
